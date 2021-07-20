52d69f8c92cba36643d835f70505a4d0bdb6ade9
Prabhakar Mishra <prabhakar.mishra@outlook.com>
2021-07-21 00:53:44 +0200

    added sqlite, apollo server connection

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,6 +1,6 @@
-import express from 'express';
+import express from "express";
 const router = express.Router();
 
-router.get('/', (_req, res) => res.send('Welcome'));
+router.get("/", (_req, res) => res.send("Welcome"));
 
 export default router;
--- src/server.ts
+++ src/server.ts
@@ -1,28 +1,35 @@
-import 'dotenv/config';
-import 'reflect-metadata';
-import express from 'express';
-// import { ApolloServer } from "apollo-server-express";
-// import { buildSchema } from "type-graphql";
-// import { createConnection } from "typeorm";
-import cookieParser from 'cookie-parser';
-// import { verify } from "jsonwebtoken";
-import cors from 'cors';
-import router from './routes';
+import "dotenv/config";
+import "reflect-metadata";
+import express from "express";
+import { ApolloServer } from "apollo-server-express";
+import { buildSchema } from "type-graphql";
+import { createConnection } from "typeorm";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import router from "./routes";
+import { UserResolver } from "./resolvers/UserResolver";
 
 const { PORT = 4000 } = process.env;
 (async () => {
-    const app = express();
-    app.use(express.json());
-    app.use(
-        cors({
-            origin: "http://localhost:3000",
-            credentials: true,
-        })
-    );
-    app.use(cookieParser());
-    app.use("/", router);
+  const app = express();
+  app.use(express.json());
+  app.use(cors());
+  app.use(cookieParser());
+  app.use("/", router);
 
-    app.listen(PORT, () => {
-        console.log(`Server is listening at port ${PORT}`);
-    });
+  await createConnection();
+  const schema = await buildSchema({
+    resolvers: [UserResolver],
+  });
+
+  const apolloServer = new ApolloServer({
+    schema,
+    context: ({ req, res }) => ({ req, res }),
+  });
+  await apolloServer.start();
+  apolloServer.applyMiddleware({ app, cors: false });
+
+  app.listen(PORT, () => {
+    console.log(`🚀 Server ready at http://localhost:${PORT}`);
+  });
 })();
