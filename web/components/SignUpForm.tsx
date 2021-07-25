f8e784a48ca507f2b6279409b59309986dfbfaed
Prabhakar Mishra <prabhakar.mishra@outlook.com>
2021-07-25 22:36:22 +0200

    connected with registration mutation

--- web/components/SignUpForm.tsx
+++ web/components/SignUpForm.tsx
@@ -1,10 +1,43 @@
-import react from "react";
+import react, { useState } from "react";
 import { Formik, Form } from "formik";
 import * as Yup from "yup";
 import InputField from "./InputField";
 import styles from "../styles/Login.module.css";
+import { gql, useMutation } from "@apollo/client";
+
+const REGISTER = gql`
+  mutation register(
+    $password: String!
+    $email: String!
+    $lastName: String!
+    $firstName: String!
+  ) {
+    register(
+      password: $password
+      email: $email
+      lastName: $lastName
+      firstName: $firstName
+    )
+  }
+`;
 
 const SignUpForm = () => {
+  const [registered, setRegistered] = useState(false);
+  const [error, setError] = useState("");
+  const [register] = useMutation(REGISTER, {
+    onCompleted(data) {
+      if (data) {
+        setRegistered(data.register);
+        setError("");
+      }
+    },
+    onError(error) {
+      const { message } = { ...error };
+      console.error("Login mutation failed: ", message);
+      setError(message);
+    },
+  });
+
   return (
     <Formik
       initialValues={{ firstName: "", lastName: "", email: "", password: "" }}
@@ -16,24 +49,35 @@ const SignUpForm = () => {
           .min(8, "Must be 8 characters or more")
           .required("Required"),
       })}
-      onSubmit={(values, { setSubmitting }) => {
-        setTimeout(() => {
-          alert(JSON.stringify(values, null, 2));
-          setSubmitting(false);
-        }, 400);
+      onSubmit={(values, { setSubmitting, resetForm }) => {
+        const { firstName, lastName, email, password } = values;
+        register({ variables: { firstName, lastName, email, password } });
+        setSubmitting(false);
+        setRegistered(false);
+        resetForm({});
       }}
     >
-      <Form className={styles.form}>
-        <h1>Create Account</h1>
-        <InputField name="firstName" type="text" placeholder="First Name" />
-        <InputField name="lastName" type="text" placeholder="Last Name" />
-        <InputField name="email" type="email" placeholder="Email" />
-        <InputField name="password" type="password" placeholder="Password" />
-
-        <button type="submit" className={`${styles.button}`}>
-          Submit
-        </button>
-      </Form>
+      {({ isSubmitting }) => (
+        <Form className={styles.form}>
+          <h1>Create Account</h1>
+          <InputField name="firstName" type="text" placeholder="First Name" />
+          <InputField name="lastName" type="text" placeholder="Last Name" />
+          <InputField name="email" type="email" placeholder="Email" />
+          <InputField name="password" type="password" placeholder="Password" />
+          {isSubmitting ? (
+            <div className={styles.message}>User registration in progress...</div>
+          ) : null}
+          {registered ? (
+            <div className={styles.message}>User successfully registered.</div>
+          ) : null}
+          {error !== "" ? (
+            <div className={styles.errorMessage}>{error}</div>
+          ) : null}
+          <button type="submit" className={`${styles.button}`}>
+            Submit
+          </button>
+        </Form>
+      )}
     </Formik>
   );
 };
