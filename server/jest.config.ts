9d88b76127fc12c56093a4c4c7f44cd5d2646c59
Prabhakar Mishra <prabhakar.mishra@outlook.com>
2021-07-27 00:02:13 +0200

    cleanup

--- server/jest.config.ts
+++ server/jest.config.ts
@@ -1,11 +1,7 @@
-// const { pathsToModuleNameMapper } = require('ts-jest/utils');
-// const { compilerOptions } = require('./tsconfig.json');
-
-module.exports = {
+export default {
   preset: 'ts-jest',
   testEnvironment: 'node',
   clearMocks: true,
   maxWorkers: 1,
-  // moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, { prefix: '<rootDir>/' }),
   testMatch: ['**/?(*.)+(spec|test).[jt]s?(x)']
 };
